88049fab16b453a55a15893a0285483823f3bc0a
Федор <68745603+frd21313123123@users.noreply.github.com>
2026-08-19 15:37:28 +0500

    chore: bump assembly version to 0.4.1

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PersistentSRBSmoke")]
-[assembly: AssemblyDescription("Persistent volumetric-inspired SRB smoke trails for Kerbal Space Program 1")]
+[assembly: AssemblyDescription("Persistent volumetric SRB smoke trails for Kerbal Space Program 1")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PersistentSRBSmoke")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e25fe49f-95a4-4af8-99c8-714bdb8e3b52")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
